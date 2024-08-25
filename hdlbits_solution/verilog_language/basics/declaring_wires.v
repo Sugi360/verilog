1b955621b9d0860f18420d3fbac6e7132628b154
Sugi360 <146251229+Sugi360@users.noreply.github.com>
2024-08-25 19:02:50 +0530

    Update declaring_wires.v

--- hdlbits_solution/verilog_language/basics/declaring_wires.v
+++ hdlbits_solution/verilog_language/basics/declaring_wires.v
@@ -1,1 +1,15 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    wire x,y;
+    assign out_n = ~out;
+    assign out = x | y;
+    assign x = a & b;
+    assign y = c & d;
+
+endmodule
 
