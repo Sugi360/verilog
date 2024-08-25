8a3ceb89c89f51852a436613e43b8b39b8908c09
Sugi360 <146251229+Sugi360@users.noreply.github.com>
2024-08-25 19:07:14 +0530

    Update 7458_chip.v

--- hdlbits_solution/verilog_language/basics/7458_chip.v
+++ hdlbits_solution/verilog_language/basics/7458_chip.v
@@ -1,1 +1,16 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    wire w,x,y,z;
+    assign w = p1a & p1b & p1c;
+    assign x = p2a & p2b;
+    assign y = p2c & p2d;
+    assign z = p1d & p1e & p1f;
+    assign p2y = x | y;
+    assign p1y = w | z;
+
+
+endmodule
 
