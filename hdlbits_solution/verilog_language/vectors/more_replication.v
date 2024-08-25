a8413afcb85e0d45c9d26d80d9ed3561994c1c5b
Sugi360 <146251229+Sugi360@users.noreply.github.com>
2024-08-25 21:13:01 +0530

    Update more_replication.v

--- hdlbits_solution/verilog_language/vectors/more_replication.v
+++ hdlbits_solution/verilog_language/vectors/more_replication.v
@@ -1,1 +1,15 @@
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
 
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs.
+    // assign out = ~{ ... } ^ { ... };
+    wire [24:0] concat1;
+    wire [24:0] concat2;
+    assign concat1 = {{5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}}};
+    assign concat2 = {5{a, b, c, d, e}};
+    assign out = ~concat1 ^ concat2;
+
+
+endmodule
