acb19781e8ead775b42456e2f7dd8e5ee513d07d
Sugi360 <146251229+Sugi360@users.noreply.github.com>
2024-09-17 21:33:35 +0530

    Update three_ports.v

--- hdlbits_solution/verilog_language/Modules:Heirarchy/three_ports.v
+++ hdlbits_solution/verilog_language/Modules:Heirarchy/three_ports.v
@@ -1,1 +1,8 @@
+module top_module ( input clk, input d, output q );
+    wire w1,w2;
+    my_dff (.q(w1), .clk(clk), .d(d));
+    my_dff (.q(w2), .clk(clk), .d(w1));
+    my_dff (.q(q), .clk(clk), .d(w2));
+
+endmodule
 
